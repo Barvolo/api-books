baa7ca59eaf902c779a19e4fb5e6573eb58d6344
Bar Volovski <bar.volovski@post.runi.ac.il>
2024-05-25 17:14:51 +0300

    update to mongo db books-service

--- books-service/app/__init__.py
+++ books-service/app/__init__.py
@@ -1,10 +1,20 @@
 from flask import Flask
+from flask_pymongo import PyMongo
 from .routes import api_blueprint
 from .rating import ratings_blueprint
+import os
 
 def create_app():
     app = Flask(__name__)
-    app.register_blueprint(api_blueprint)
+    app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb://localhost:27017/books')
 
+    mongo = PyMongo(app)
+    app.mongo = mongo
+
+    app.register_blueprint(api_blueprint)
     app.register_blueprint(ratings_blueprint)
+
     return app
+
+
+
--- books-service/app/routes.py
+++ books-service/app/routes.py
@@ -1,16 +1,17 @@
-from flask import Blueprint, request, jsonify
+import uuid
+from flask import Blueprint, request, jsonify, current_app
 from app.models import is_request_json
 from app.rating import create_rating, delete_rating
 from .external_apis import fetch_book_details
 from urllib.parse import unquote
+from flask_pymongo import PyMongo
 
 api_blueprint = Blueprint('api', __name__)
 books = {}
-book_id = 0
 
 @api_blueprint.route('/books', methods=['POST'])
 def create_book():
-    global book_id
+    # Check if the media type is JSON
     is_valid, result = is_request_json()
     if not is_valid:
         # Return a 415 status code for all JSON related issues
@@ -24,31 +25,34 @@ def create_book():
     if data['genre'] not in ['Fiction', 'Children', 'Biography', 'Science', 'Science Fiction', 'Fantasy','Other']:
         return jsonify({"error": "Invalid genre"}), 422
     
-    # Check if the ISBN is already in the books
-    for book in books.values():
-        if book['ISBN'] == data['ISBN']:
-            return jsonify({"error": "Book with this ISBN already exists"}), 422
+    # Check if the ISBN is already in MongoDB
+    existing_book = current_app.mongo.db.books.find_one({"ISBN": data['ISBN']})
+    if existing_book:
+        return jsonify({"error": "Book with this ISBN already exists"}), 422
+    
     
     # Increment book ID here so that it starts from 1
     try:
-        book_id += 1
-        
         # Fetch book details from external sources
         book_details = fetch_book_details(data['ISBN'])
         
         if book_details is None:
             # If no details are found, decrement the book_id and respond with error
-            book_id -= 1
+            #book_id -= 1
             return jsonify({"error": "no items returned from Google Book API for given ISBN number"}), 400
         # Combine the provided data with fetched details and store in the books dictionary
-        books[book_id] = {**data, **book_details, "id": str(book_id)}
-
+        data.update(book_details)
+        # Insert the book data into MongoDB and get the inserted_id
+        data['id'] = str(uuid.uuid4())
+        current_app.mongo.db.books.insert_one(data)
+        
+        #data['id'] = str(inserted_book)  # Using MongoDB's generated ID as 'id'
         # Print to console for debugging (can be removed in production)
-        print(f'Book ID {book_id} added:', books[book_id])
+        #print(f'Book ID {book_id} added:', books[book_id])
 
-        create_rating(books[book_id])
+        create_rating(data)
         # Return the new book record with status code 201 (Created)
-        return jsonify({"id": str(book_id), "status code": str(201)}), 201  # Respond with string ID as per requirement
+        return jsonify({"id": data['id'], "status code": str(201)}), 201  # Respond with string ID as per requirement
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
@@ -56,12 +60,21 @@ def create_book():
 def get_books():
     SUPPORTED_LANGUAGES = ['heb', 'eng', 'spa', 'chi']
     query_params = request.query_string.decode("utf-8")
-    all_books = list(books.values())
+    all_books = []
     result_books = []  # List to store final filtered books
+
+    # Fetch all books from MongoDB
+    try:
+        mongo_books_cursor = current_app.mongo.db.books.find({}, {'_id': 0})
+        all_books = list(mongo_books_cursor)
+    except Exception as e:
+        current_app.logger.error(f"Failed to fetch books from MongoDB: {str(e)}")
+        return jsonify({"error": "Failed to fetch books"}), 500
+    
     try:
         if query_params == '':
             return jsonify(all_books), 200
-        for book_id, book_data in books.items():
+        for book_data in all_books:
             result_books.append(book_data)
             for query in query_params.split('&'):
                 #if '%20contains%20' in query:
@@ -84,29 +97,38 @@ def get_books():
         
     return jsonify(result_books), 200
   
-@api_blueprint.route('/books/<int:book_id>', methods=['GET'])
+@api_blueprint.route('/books/<string:book_id>', methods=['GET'])
 def get_book(book_id):
     # Attempt to retrieve the book by ID
     try:
-        book = books.get(book_id)
+        book = current_app.mongo.db.books.find_one({"id": book_id})
         if book:
+            if '_id' in book:
+                book['_id'] = str(book['_id'])
             return jsonify(book), 200
         else:
             return jsonify({"error": "Book not found"}), 404
     except:
         return jsonify({"error": "Book not found"}), 404
 
-@api_blueprint.route('/books/<int:book_id>', methods=['DELETE'])
+@api_blueprint.route('/books/<string:book_id>', methods=['DELETE'])
 def delete_book(book_id):
-    book = books.get(book_id)
-    if book:
-        del books[book_id]
-        delete_rating(book_id)
-        return jsonify({"status": "Book deleted"}), 200
-    else:
-        return jsonify({"error": "Book not found"}), 404
-
-@api_blueprint.route('/books/<int:book_id>', methods=['PUT'])
+    try:
+        # Perform the deletion and check the result
+        result = current_app.mongo.db.books.delete_one({"id": book_id})
+        if result.deleted_count == 0:
+            # If no documents were deleted, return a 404 response
+            return jsonify({"error": "Book not found"}), 404
+        else:
+            # If a document was deleted, proceed to delete related ratings
+            delete_rating(book_id)
+            return jsonify({"status": "Book deleted"}), 200
+    except Exception as e:
+        # If there's an exception during the delete operation, log it and return a 500 error
+        current_app.logger.error(f"Failed to delete book with ID {book_id}: {str(e)}")
+        return jsonify({"error": "Internal Server Error"}), 500
+    
+@api_blueprint.route('/books/<string:book_id>', methods=['PUT'])
 def update_book(book_id):
     # Check if the media type is JSON
     is_valid, result = is_request_json()
@@ -114,6 +136,37 @@ def update_book(book_id):
         # Return a 415 status code for all JSON related issues
         return jsonify(result), 415
     
+    # Check if the book exists
+    book = current_app.mongo.db.books.find_one({"id": book_id})
+    if not book:
+        return jsonify({"error": "Book not found"}), 404
+    
+    # Get the update data
+    update_data = request.json
+    required_fields = ['title', 'authors', 'ISBN', 'genre', 'publisher', 'publishedDate', 'id']
+
+    # Check if all required fields are in the update data
+    if not all(field in update_data for field in required_fields):
+        return jsonify({"error": "Missing fields, all fields must be provided or invalid argument"}), 422
+    
+    # Check if length of update data is correct
+    if len(update_data) != 7:
+        return jsonify({"error": "Too many arguments"}), 422
+    
+    # Check if the genre is valid
+    if update_data['genre'] not in ['Fiction', 'Children', 'Biography', 'Science', 'Science Fiction', 'Fantasy','Other']:
+        return jsonify({"error": "Invalid genre"}), 422
+    
+    # Update the book
+    result = current_app.mongo.db.books.update_one({"id": book_id}, {"$set": update_data})
+    if result.matched_count == 0:
+        return jsonify({"error": "Book not found"}), 404
+    if result.modified_count == 0:
+        return jsonify({"error": "No updates performed"}), 304  # Not Modified
+    return jsonify({"id": book_id}), 200
+    
+
+    '''
     # Check if the book exists
     if book_id not in books.keys():
         return jsonify({"error": "Book not found"}), 404
@@ -148,3 +201,4 @@ def update_book(book_id):
     }
     return jsonify({"id": book_id}), 200
 
+    '''
